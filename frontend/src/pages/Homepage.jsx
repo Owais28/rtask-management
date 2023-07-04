0c684ffac4b76299d0ba28bd9345790254ada8df
Owais Athar <owaisathar2882@gmail.com>
2023-07-05 00:48:56 +0530

    done

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -71,15 +71,17 @@ const HomePage = () => {
       bg={useColorModeValue("gray.800", "gray.800")}
     >
       <Stack position={'relative'} spacing={8} maxW={1000} mx={"auto"} flexBasis={1} py={12} px={1}>
-        <HStack wrap={'wrap'} rowGap={5} alignItems={'center'} justifyContent={'space-between'} px={3}>
+        <HStack wrap={'wrap'} transition={'all .3s linear'} rowGap={5} alignItems={'center'} justifyContent={'space-between'} px={3}>
           <Heading fontFamily={'Space Mono'} fontWeight={'black'} position={'static'} color={'white'} as="h1" fontSize={['2xl', '4xl']} transition={'all .5s linear'}>
             Tasks
           </Heading>
-          <HStack gap={10}>
-            <HStack as={'button'} alignItems={'center'} >
-              <Avatar size={['xs', 'sm']} /> <Text color={'white'} fontSize={['sm', 'md']} >Owais Athar</Text>
+          <HStack gap={[5, 6, 8]}>
+
+            <HStack as={'button'} alignItems={'center'} transition={'all .5s linear'}>
+              <Avatar transition={'all .5s linear'} size={['xs', 'sm']} /> <Text transition={'all .5s linear'} color={'white'} fontSize={['sm', 'md']} >Owais Athar</Text>
             </HStack>
-            <Button colorScheme='yellow' size={['xs', 'md']}>
+
+            <Button colorScheme='yellow' size={['xs', 'md']} >
               Logout
             </Button>
           </HStack>
