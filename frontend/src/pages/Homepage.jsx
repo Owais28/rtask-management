c3dd2eddd9851722635026a264e5bc7ef00d01d4
Owais Athar <owaisathar2882@gmail.com>
2023-07-05 00:08:54 +0530

    done

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Button, Heading, Stack, Text, useColorModeValue, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Grid, HStack, Input, Textarea, Select, Badge, Spinner, Box } from '@chakra-ui/react';
+import { Button, Heading, Stack, Text, useColorModeValue, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Grid, HStack, Input, Textarea, Select, Badge, Spinner, Box, MenuOptionGroup, MenuItem, Menu, MenuButton, MenuList, MenuItemOption, MenuDivider, MenuIcon, Avatar, VStack } from '@chakra-ui/react';
 import TaskCard from './components/TaskCard';
 
 const HomePage = () => {
@@ -71,9 +71,19 @@ const HomePage = () => {
       bg={useColorModeValue("gray.800", "gray.800")}
     >
       <Stack position={'relative'} spacing={8} maxW={1000} mx={"auto"} flexBasis={1} py={12} px={1}>
-        <Heading position={'static'} color={useColorModeValue('white')} px={3} as="h1" mb={4} fontSize={['2xl', '4xl']} transition={'all .5s linear'}>
-          Task List
-        </Heading>
+        <HStack wrap={'wrap'} rowGap={5} alignItems={'center'} justifyContent={'space-between'} px={3}>
+          <Heading position={'static'} color={useColorModeValue('white')} as="h1" fontSize={['2xl', '4xl']} transition={'all .5s linear'}>
+            Task List
+          </Heading>
+          <HStack gap={10}>
+            <HStack as={'button'} alignItems={'center'} >
+              <Avatar size={'sm'} /> <Text color={'white'} >Owais Athar</Text>
+            </HStack>
+            <Button colorScheme='yellow' size={['sm', 'md']}>
+              Logout
+            </Button>
+          </HStack>
+        </HStack>
         <Grid transition={'all .5s linear'} px={3} autoRows={'auto'} templateColumns={['repeat(1, fr)', 'repeat(2, 200px)', 'repeat(3, 1fr)']} overflow={'scroll'} gap={3}>
           {tasks.length > 0 ? (
             [...tasks, ...tasks, ...tasks].map((task, index) => (
@@ -157,15 +167,14 @@ const HomePage = () => {
               <ModalHeader fontFamily={'Roboto'} fontSize={['lg', 'xl']}>Task 1</ModalHeader>
               <ModalCloseButton />
               <ModalBody fontFamily={'Roboto'} fontSize={['md', 'lg']} height={'max-content'}>
-                <Stack>
+                <Stack fontSize={'sm'}>
                   <HStack gap={10}>
                     <Text width={'25%'}>
                       Status
                     </Text>
-                    <Select placeholder='Select option' defaultValue={'TO-DO'}>
+                    <Select placeholder='Select option' fontSize={'sm'} defaultValue={'TO-DO'}>
                       <option value='TO-DO'>
                         TO-DO
-
                       </option>
                       <option value='IN-PROGRESS'>IN-PROGRESS</option>
                       <option value='DONE'>DONE</option>
@@ -181,11 +190,39 @@ const HomePage = () => {
                     <Text width={'25%'}>
                       Assignee
                     </Text>
-                    <Select placeholder='Select option' fill>
-                      <option value='option1'>Option 1</option>
-                      <option value='option2'>Option 2</option>
-                      <option value='option3'>Option 3</option>
-                    </Select>
+                    <Menu closeOnSelect={false}>
+                      <MenuButton as={Button} colorScheme='gray' width={'full'}>
+                        Select
+                      </MenuButton>
+                      <MenuList minWidth='240px'>
+                        <MenuOptionGroup title='Users' type='checkbox'>
+                          <MenuItemOption value='email'>
+                            <HStack alignItems={'center'} justify={'start'}>
+                              <Avatar size={'sm'} mr={1} />
+                              <Text fontSize={'sm'}>
+                                Owais Athar
+                              </Text>
+                            </HStack>
+                          </MenuItemOption>
+                          <MenuItemOption value='phone'>
+                            <HStack alignItems={'center'} justify={'start'}>
+                              <Avatar size={'sm'} mr={1} />
+                              <Text fontSize={'sm'}>
+                                Owais Athar
+                              </Text>
+                            </HStack>
+                          </MenuItemOption>
+                          <MenuItemOption value='country'>
+                            <HStack alignItems={'center'} justify={'start'}>
+                              <Avatar size={'sm'} mr={1} />
+                              <Text fontSize={'sm'}>
+                                Owais Athar
+                              </Text>
+                            </HStack>
+                          </MenuItemOption>
+                        </MenuOptionGroup>
+                      </MenuList>
+                    </Menu>
                   </HStack>
                   {/* <Lorem count={2} /> */}
                   <Stack>
@@ -205,7 +242,87 @@ const HomePage = () => {
                 <Button variant='outline' colorScheme='red'>Delete</Button>
               </ModalFooter>
             </>
-          ) : (<Text>sds</Text>)
+          ) : (<>
+            <ModalHeader fontFamily={'Roboto'} fontSize={['lg', 'xl']}>
+              <Input placeholder='give title' width={'90%'} colorScheme='orange' />
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody fontFamily={'Roboto'} fontSize={['md', 'lg']} height={'max-content'}>
+              <Stack fontSize={'sm'}>
+                <HStack gap={10}>
+                  <Text width={'25%'}>
+                    Status
+                  </Text>
+                  <Select placeholder='Select option' defaultValue={'TO-DO'}>
+                    <option value='TO-DO'>
+                      TO-DO
+                    </option>
+                    <option value='IN-PROGRESS'>IN-PROGRESS</option>
+                    <option value='DONE'>DONE</option>
+                  </Select>
+                </HStack>
+                <HStack gap={10}>
+                  <Text width={'25%'}>
+                    Due date
+                  </Text>
+                  <Input type='date' placeholder='due date' />
+                </HStack>
+                <HStack gap={10}>
+                  <Text width={'25%'}>
+                    Assignee
+                  </Text>
+                  <Menu closeOnSelect={false}>
+                    <MenuButton as={Button} colorScheme='gray' width={'full'}>
+                      Select
+                    </MenuButton>
+                    <MenuList minWidth='240px'>
+                      <MenuOptionGroup title='Users' type='checkbox'>
+                        <MenuItemOption value='email'>
+                          <HStack alignItems={'center'} justify={'start'}>
+                            <Avatar size={'sm'} mr={1} />
+                            <Text fontSize={'sm'}>
+                              Owais Athar
+                            </Text>
+                          </HStack>
+                        </MenuItemOption>
+                        <MenuItemOption value='phone'>
+                          <HStack alignItems={'center'} justify={'start'}>
+                            <Avatar size={'sm'} mr={1} />
+                            <Text fontSize={'sm'}>
+                              Owais Athar
+                            </Text>
+                          </HStack>
+                        </MenuItemOption>
+                        <MenuItemOption value='country'>
+                          <HStack alignItems={'center'} justify={'start'}>
+                            <Avatar size={'sm'} mr={1} />
+                            <Text fontSize={'sm'}>
+                              Owais Athar
+                            </Text>
+                          </HStack>
+                        </MenuItemOption>
+                      </MenuOptionGroup>
+                    </MenuList>
+                  </Menu>
+                </HStack>
+                {/* <Lorem count={2} /> */}
+                <Stack>
+                  <Text>
+                    Description
+                  </Text>
+                  <Textarea bgColor={'gray.100'} />
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus cum voluptas ipsam vitae illo cumque nihil, accusamus maiores, dolorum eum nostrum ut id deserunt tempore, nemo esse rem nam.
+                </Stack>
+              </Stack>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Update
+              </Button>
+              <Button variant='outline' colorScheme='red'>Delete</Button>
+            </ModalFooter>
+          </>)
           }
         </ModalContent>
       </Modal>
