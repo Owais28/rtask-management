39f31aa83cb5f558fbf4f437ddb1a51332ed9a2d
Owais Athar <owaisathar2882@gmail.com>
2023-07-04 21:44:28 +0530

    done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Button, Flex, Heading, Stack, Text, useColorModeValue, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Grid } from '@chakra-ui/react';
+import { Button, Heading, Stack, Text, useColorModeValue, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Grid, HStack, Input, Textarea, Select, Badge, Spinner, Box } from '@chakra-ui/react';
 import TaskCard from './components/TaskCard';
 
 const HomePage = () => {
@@ -47,6 +47,7 @@ const HomePage = () => {
   }, []);
 
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpenNewTask, onOpenNewTask, onCloseNewTask } = useDisclosure()
 
   return (
     <Stack minH={"100vh"}
@@ -58,7 +59,7 @@ const HomePage = () => {
 
       bg={useColorModeValue("gray.800", "gray.800")}
     >
-      <Stack spacing={8} maxW={1000} mx={"auto"} flexBasis={1} py={12} px={1}>
+      <Stack position={'relative'} spacing={8} maxW={1000} mx={"auto"} flexBasis={1} py={12} px={1}>
         <Heading position={'static'} color={useColorModeValue('white')} px={3} as="h1" mb={4} fontSize={['2xl', '4xl']} transition={'all .5s linear'}>
           Task List
         </Heading>
@@ -71,23 +72,124 @@ const HomePage = () => {
             <Text>No tasks found.</Text>
           )}
         </Grid>
+        <Button colorScheme='orange' position={'fixed'} onClick={onOpenNewTask} bottom={10} right={10} as='button' maxWidth={1000} mx={"auto"} >
+          <Text fontSize={'xl'} >
+            Create +
+          </Text>
+        </Button>
       </Stack>
-      <Modal isOpen={isOpen} onClose={onClose} isCentered >
+
+      <Modal isOpen={isOpenNewTask} onClose={onCloseNewTask} isCentered >
         <ModalOverlay backdropFilter="blur(8px)"   // Apply the blur effect to the overlay
           bg="rgba(0, 0, 0, 0.5)" />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+        <ModalContent width={['90vw']}>
+          <ModalHeader fontFamily={'Roboto'} fontSize={['lg', 'xl']}>Task 1</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            {/* <Lorem count={2} /> */}
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus cum voluptas ipsam vitae illo cumque nihil, accusamus maiores, dolorum eum nostrum ut id deserunt tempore, nemo esse rem nam.
+          <ModalBody fontFamily={'Roboto'} fontSize={['md', 'lg']} height={'max-content'}>
+            <Stack>
+              <HStack gap={10}>
+                <Text width={'25%'}>
+                  Status
+                </Text>
+                <Select placeholder='Select option' defaultValue={'TO-DO'}>
+                  <option value='TO-DO'>
+                    TO-DO
+
+                  </option>
+                  <option value='IN-PROGRESS'>IN-PROGRESS</option>
+                  <option value='DONE'>DONE</option>
+                </Select>
+              </HStack>
+              <HStack gap={10}>
+                <Text width={'25%'}>
+                  Due date
+                </Text>
+                <Input type='date' placeholder='due date' />
+              </HStack>
+              <HStack gap={10}>
+                <Text width={'25%'}>
+                  Assignee
+                </Text>
+                <Select placeholder='Select option' fill>
+                  <option value='option1'>Option 1</option>
+                  <option value='option2'>Option 2</option>
+                  <option value='option3'>Option 3</option>
+                </Select>
+              </HStack>
+              {/* <Lorem count={2} /> */}
+              <Stack>
+                <Text>
+                  Description
+                </Text>
+                <Textarea bgColor={'gray.100'} />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus cum voluptas ipsam vitae illo cumque nihil, accusamus maiores, dolorum eum nostrum ut id deserunt tempore, nemo esse rem nam.
+              </Stack>
+            </Stack>
           </ModalBody>
 
           <ModalFooter>
             <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
+              Update
             </Button>
-            <Button variant='ghost'>Secondary Action</Button>
+            <Button variant='outline' colorScheme='red'>Delete</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      {/* Edit Modal */}
+      <Modal isOpen={isOpen} onClose={onClose} isCentered >
+        <ModalOverlay backdropFilter="blur(8px)"   // Apply the blur effect to the overlay
+          bg="rgba(0, 0, 0, 0.5)" />
+        <ModalContent width={['90vw']}>
+          <ModalHeader fontFamily={'Roboto'} fontSize={['lg', 'xl']}>Task 1</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody fontFamily={'Roboto'} fontSize={['md', 'lg']} height={'max-content'}>
+            <Stack>
+              <HStack gap={10}>
+                <Text width={'25%'}>
+                  Status
+                </Text>
+                <Select placeholder='Select option' defaultValue={'TO-DO'}>
+                  <option value='TO-DO'>
+                    TO-DO
+
+                  </option>
+                  <option value='IN-PROGRESS'>IN-PROGRESS</option>
+                  <option value='DONE'>DONE</option>
+                </Select>
+              </HStack>
+              <HStack gap={10}>
+                <Text width={'25%'}>
+                  Due date
+                </Text>
+                <Input type='date' placeholder='due date' />
+              </HStack>
+              <HStack gap={10}>
+                <Text width={'25%'}>
+                  Assignee
+                </Text>
+                <Select placeholder='Select option' fill>
+                  <option value='option1'>Option 1</option>
+                  <option value='option2'>Option 2</option>
+                  <option value='option3'>Option 3</option>
+                </Select>
+              </HStack>
+              {/* <Lorem count={2} /> */}
+              <Stack>
+                <Text>
+                  Description
+                </Text>
+                <Textarea bgColor={'gray.100'} />
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus cum voluptas ipsam vitae illo cumque nihil, accusamus maiores, dolorum eum nostrum ut id deserunt tempore, nemo esse rem nam.
+              </Stack>
+            </Stack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Update
+            </Button>
+            <Button variant='outline' colorScheme='red'>Delete</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
